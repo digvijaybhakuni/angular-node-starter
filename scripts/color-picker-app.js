760ce9e7855f5822f7c79f71c4ccfefc12e11884
Digvijay IGGLOBAL <digvijay.bhakuni@rediffmail.com>
2015-12-09 18:09:17 +0530

    Directive and factory

--- scripts/color-picker-app.js
+++ scripts/color-picker-app.js
@@ -43,3 +43,12 @@ colorPickerApp.directive('colorPicker', function () {
         }
     };
 });
+
+
+//<<<<<<<<<<<<<<<Read Isolated Scopes >>>>>>>>>>>>>>
+
+/*Listing 2.38 shows how you can establish a number of connections to the parent scope, despite using an isolated scope. As such, you can access the data and functions of the parent scope. The main factor is that you no longer assign an empty object to the scope property of the DDO. Instead, you specify a configuration object. The key in this configuration object is how you specify which scope variables from the directive’s isolated scope should connect to the parent scope. As already mentioned, there are basically three forms. These are specified by the prefixes =, @ and &.
+
+=. Indicates that the scope variable in the isolated scope of the directive that is specified as the key is to establish a two-way data binding to a variable in the parent scope.
+@. Indicates that the scope variable in the directive’s isolated scope that is specified as the key is to establish a one-way data binding to the parent scope.
+&. Indicates that the scope variable specified as the key should reference a function in the parent scope and be able to invoke the function and pass internal data from the directive.*/
